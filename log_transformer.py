44cd59694bd1a6ba6043cd075bb50d1a6ad45f79
easydatain <74894256+easydatain@users.noreply.github.com>
2020-11-23 20:06:27 +0530

    Create log_transformer.py

---
+++ log_transformer.py
@@ -0,0 +1,19 @@
+params={'column':'Score'}
+import pandas as pd
+import math
+import numpy as np
+import json
+import _helper
+def main():
+    try:
+        data = _helper.data() 
+        if params['column'] in data:
+            data['logarithm_base10'] = np.log10(data[params['column']])
+            return  _helper.publish(data)
+          
+        else:
+            print("column doesn't exist")
+               
+    except Exception as e:
+          print(e)
+        
