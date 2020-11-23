6d09b1c9ccc5a1f2f4a59d78482114e5e98047dd
easydatain <74894256+easydatain@users.noreply.github.com>
2020-11-23 19:45:58 +0530

    Create box_cox.py

---
+++ box_cox.py
@@ -0,0 +1,20 @@
+params ={'column':'Score'}
+
+from scipy import stats
+import pandas as pd
+import json
+import _helper
+def main():
+    try:
+        df = _helper.data()
+        if params['column'] in df:
+            transform = df[params['column']].values
+            # transform values and store as "dft"
+            dft = stats.boxcox(transform)
+            df['box_cox'] = dft[0]
+            return _helper.publish(df)
+        else:
+            return 'Column does not exist'
+        
+    except Exception as e:
+        print(e)
