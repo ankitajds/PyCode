b46576f488e58ecdb5850dc7f607c22f2277f5f8
easydatain <74894256+easydatain@users.noreply.github.com>
2020-11-23 18:29:53 +0530

    Create numerical_operations.py

---
+++ numerical_operations.py
@@ -0,0 +1,34 @@
+params =  {"columns" : ['Age','CarLoan','Balance'],'oper':'div'}
+_helper.tablename='my new filename'
+
+
+import pandas as pd
+import json
+import _helper
+
+
+def main():
+    try:
+        df = _helper.data()
+        for col in params['columns'][0:1]:
+            if params['oper']=='add':
+                df['add'] = df[col]
+            elif params['oper']=='sub':
+                df['sub'] = df[col]
+            elif params['oper']=='mul':
+                df['mul'] = df[col]
+            elif params['oper']=='div':
+                df['div'] = df[col]
+            for col in params['columns'][1:]:
+                if params['oper']=='add':
+                    df['add'] += df[col]
+                elif params['oper']=='sub':
+                    df['sub'] -= df[col]
+                elif params['oper']=='mul':
+                    df['mul'] *= df[col]
+                elif params['oper']=='div':
+                    df['div'] /= df[col]
+        return _helper.publish(data)
+    except Exception as e:
+        print(e)
+         
