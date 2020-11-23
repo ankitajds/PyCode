54e124241d0fcd34b11b0342f445fb7ca72b8e55
easydatain <74894256+easydatain@users.noreply.github.com>
2020-11-23 18:34:26 +0530

    Create neg_pos_count.py

---
+++ neg_pos_count.py
@@ -0,0 +1,24 @@
+params =  {"var" : ""}
+_helper.tablename='my new filename'
+
+import pandas as pd
+import math
+import numpy as np
+import json
+import _helper
+
+def main():
+    try:
+        data = _helper.data()
+        if params["var"]   == 'positive':
+            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
+        elif params["var"] == 'negative':
+            data['negative_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
+        elif params["var"] == 'both':
+            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
+            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
+            
+        return _helper.publish(data)
+     
+    except Exception as e:
+        print(e)
