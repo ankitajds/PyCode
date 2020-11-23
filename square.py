fe2644be16e770ff807cd5df21fe857b99ac0e58
easydatain <74894256+easydatain@users.noreply.github.com>
2020-11-23 20:43:01 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,26 @@
+#Enter column names in the list defined below
+# pass the integer value in n variable for find square
+
+params={'column':['Age'],'m':2}
+
+import pandas as pd
+import math
+import json
+import _helper
+def main():
+    try:
+        df = _helper.data()
+        for col in params['column']:
+            try:
+                df[col+"_n_root"]=df[col]**params['m']
+                return _helper.publish(df)
+
+            except:
+                return 'Enter column names which have float  datatype'
+
+
+    except Exception as e:
+        print(e)
+
+    
+                    
