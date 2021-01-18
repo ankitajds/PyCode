9d807faa279a5f70a3e184de3391c98d957bb681
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-18 18:24:25 +0530

    Update Sentiment.py

--- Sentiment.py
+++ Sentiment.py
@@ -1,5 +1,6 @@
 params =  {"column" :''}
-_helper.tablename='my new filename'
+#_helper.userdefinedTablename="ybl_banks"
+#_helper.fileid=5
 
 import pandas as pd
 import re
@@ -9,59 +10,61 @@ import json
 import _helper
 
 def main():
-    try:
-        df = _helper.data()
-        #clean html
-        def cleanhtml(sent):
-            clean = re.sub(r'<*?>', r' ', sent)
-            return clean
+    df = _helper.data()
+    #clean html
+    def cleanhtml(sent):
+        clean = re.sub(r'<*?>', r' ', sent)
+        return clean
 
-        #clean punctuation
-        def cleanpunc(word):
-            cleanr = re.sub(r'[?|!|\'|"|#]', r' ', word)
-            cleaned = re.sub(r'[)|(|\|/]', r' ', cleanr)
-            return cleaned
+    #clean punctuation
+    def cleanpunc(word):
+        cleanr = re.sub(r'[?|!|\'|"|#]', r' ', word)
+        cleaned = re.sub(r'[)|(|\|/]', r' ', cleanr)
+        return cleaned
 
-        lst_of_sent = []
-        str1 = ''
-        final_str = []
+    lst_of_sent = []
+    str1 = ''
+    final_str = []
 
-        if params['column'] in df:
-            for sent in df[params['column']].values:
-                filtered_sent = []
-                sent          = cleanhtml(sent)
-                for word in sent.split():
-                    for clean_words in cleanpunc(word).split():
-                        if (clean_words.isalpha()):
-                            filtered_sent.append(clean_words.lower())
-                lst_of_sent.append(filtered_sent)
-        else:
-            print("Column does not exist ")
+    if params['column'] in df:
+        for sent in df[params['column']].values:
+            filtered_sent = []
+            sent          = cleanhtml(sent)
+            for word in sent.split():
+                for clean_words in cleanpunc(word).split():
+                    if (clean_words.isalpha()):
+                        filtered_sent.append(clean_words.lower())
+            lst_of_sent.append(filtered_sent)
+    else:
+        print("Column does not exist ")
 
-        for lst in lst_of_sent:
-            str1 = ' '.join(lst)
-            final_str.append(str1)
+    for lst in lst_of_sent:
+        str1 = ' '.join(lst)
+        final_str.append(str1)
 
-        df['clean_Text'] = final_str
+    df['clean_Text'] = final_str
 
-        #Extract sentiment from text
+    #Extract sentiment from text
+
+    analyzer = SentimentIntensityAnalyzer()
+    lst_of_sent = []
+    for sentence in final_str:
+        vs = analyzer.polarity_scores(sentence)
+        for item in vs.items():
+            for key in item:
+                if key == 'compound':
+                    lst_of_sent.append(vs[key])
+                else:
+                    pass
+
+    df['compound'] = lst_of_sent
+    _helper.status(fileid,2,'')
+    return _helper.publish(df)
         
-        analyzer = SentimentIntensityAnalyzer()
-        lst_of_sent = []
-        for sentence in final_str:
-            vs = analyzer.polarity_scores(sentence)
-            for item in vs.items():
-                for key in item:
-                    if key == 'compound':
-                        lst_of_sent.append(vs[key])
-                    else:
-                        pass
-
-        df['compound'] = lst_of_sent
-        _helper.status(fileid,2,'')
-        return _helper.publishbot(df)
-        
-    except Exception as e:
-        _helper.status(fileid,-2,e)
+try:
+    if main() != None:
+        _helper.status(_helper.fileid, 2,'Completed')
+except Exception as e:
+    _helper.status(_helper.fileid, -2,str(e))
         
         
