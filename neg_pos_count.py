a01e9db39380eec25ddf6d0e9c5ad481c2d686ed
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-18 18:31:10 +0530

    Update neg_pos_count.py

--- neg_pos_count.py
+++ neg_pos_count.py
@@ -8,18 +8,19 @@ import json
 import _helper
 
 def main():
-    try:
-        data = _helper.data()
-        if params["var"]   == 'positive':
-            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
-        elif params["var"] == 'negative':
-            data['negative_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
-        elif params["var"] == 'both':
-            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
-            data['positive_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
-            
-        _helper.status(fileid,2,'')
-        return _helper.publishbot(data)
+    data = _helper.data()
+    if params["var"]   == 'positive':
+        data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
+    elif params["var"] == 'negative':
+        data['negative_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
+    elif params["var"] == 'both':
+        data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
+        data['positive_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
+
+    return _helper.publish(data)
         
-    except Exception as e:
-        _helper.status(fileid,-2,e)
+try:
+    if main() != None:
+        _helper.status(_helper.fileid, 2,'Completed')
+except Exception as e:
+    _helper.status(_helper.fileid, -2,str(e))
