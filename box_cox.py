16ca1dee31bf97207f9ae2ff4325ea6d8922bbeb
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-18 18:25:32 +0530

    Update box_cox.py

--- box_cox.py
+++ box_cox.py
@@ -6,19 +6,21 @@ import pandas as pd
 import json
 import _helper
 def main():
-    try:
-        df = _helper.data()
-        if params['column'] in df:
-            transform = df[params['column']].values
-            # transform values and store as "dft"
-            dft = stats.boxcox(transform)
-            df['box_cox'] = dft[0]
-            return _helper.publish(df)
-        else:
-            return 'Column does not exist'
+    df = _helper.data()
+    if params['column'] in df:
+        transform = df[params['column']].values
+        # transform values and store as "dft"
+        dft = stats.boxcox(transform)
+        df['box_cox'] = dft[0]
+        return _helper.publish(df)
+    else:
+        return 'Column does not exist'
+
+    _helper.status(fileid,2,'')
+    return _helper.publish(df)
         
-        _helper.status(fileid,2,'')
-        return _helper.publishbot(df)
-        
-    except Exception as e:
-        _helper.status(fileid,-2,e)
+try:
+    if main() != None:
+        _helper.status(_helper.fileid, 2,'Completed')
+except Exception as e:
+    _helper.status(_helper.fileid, -2,str(e))
