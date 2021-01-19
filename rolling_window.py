a276dd7650a22abcf140d26aa080c7799749a9c6
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-19 10:24:56 +0530

    Update rolling_window.py

--- rolling_window.py
+++ rolling_window.py
@@ -5,6 +5,7 @@
 #Provide a operation like sum or mean.
 
 params={'columns':[],'window_size':,'window_type':,'operation': }
+import _helper
 _helper.tablename='my new filename'
 
 import pandas as pd
@@ -12,23 +13,25 @@ import numpy as np
 import re
 from pandas import read_csv
 import json
-import _helper
+
 
 def main():
-    try:
-        du = _helper.data()
-        for col in params['columns']:
-            if params['operation'] =="sum":
-                du[col+"_sum"] = du[col].rolling(params['window_size'],win_type = params['window_type']).sum()
-            elif params['operation'] =="mean":
-                du[col+"_mean"] = du[col].rolling(params['window_size'], win_type = params['window_type']).mean()
-           
+    du = _helper.data()
+    for col in params['columns']:
+        if params['operation'] =="sum":
+            du[col+"_sum"] = du[col].rolling(params['window_size'],win_type = params['window_type']).sum()
+        elif params['operation'] =="mean":
+            du[col+"_mean"] = du[col].rolling(params['window_size'], win_type = params['window_type']).mean()
 
-        _helper.status(fileid,2,'')
-        return _helper.publishbot(df)
+
+
+    return _helper.publish(du)
         
-    except Exception as e:
-        _helper.status(fileid,-2,e)
+try:
+    if main() != None:
+        _helper.status(_helper.fileid, 2,'Completed')
+except Exception as e:
+    _helper.status(_helper.fileid, -2,str(e))
         
 
    
