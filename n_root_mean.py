b84f7787e4120fdbbef8c5ccf720c31ece10ab06
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 15:31:44 +0530

    Update n_root_mean.py

--- n_root_mean.py
+++ n_root_mean.py
@@ -1,25 +1,20 @@
 #Enter column names in the list defined below
+#columns=[]
 # pass the float value in n variable for find n_root_mean value
-
-params={'column':[],'m':}
+m=
 import _helper
-#_helper.tablename='my new filename'
 import pandas as pd
 import math
 import json
 
 def main():
     df = _helper.data()
-    for col in params['column']:
+    for col in 'columns':
         try:
-            df[col+"_n_root"]=df[col]**params['m']
+            df[col+"_n_root"]=df[col]**m
             return _helper.publish(df)
 
         except:
             return 'Enter column names which have float  datatype'
 
-try:
-    if main() != None:
-        _helper.status(_helper.fileid, 2,'Completed')
-except Exception as e:
-    _helper.status(_helper.fileid, -2,str(e))
+
