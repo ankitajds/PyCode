7b97cfeb3865e1cd2a62e23e597a019143319fc4
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 15:39:47 +0530

    Update numerical_operations.py

--- numerical_operations.py
+++ numerical_operations.py
@@ -1,34 +1,31 @@
-params =  {"columns" : [],'oper':''}
+#Enter the column name in a list columns
+columns=[]
+#Enter 'add' or'sub'or'mul' or 'div' in oper
+oper=''
 import _helper
 import pandas as pd
 import json
 
 def main():
     df = _helper.data()
-    for col in params['columns'][0:1]:
-        if params['oper']=='add':
+    for col in columns[0:1]:
+        if oper =='add':
             df['add'] = df[col]
-        elif params['oper']=='sub':
+        elif oper =='sub':
             df['sub'] = df[col]
-        elif params['oper']=='mul':
+        elif oper =='mul':
             df['mul'] = df[col]
-        elif params['oper']=='div':
+        elif oper =='div':
             df['div'] = df[col]
-        for col in params['columns'][1:]:
-            if params['oper']=='add':
+        for col in columns[1:]:
+            if oper =='add':
                 df['add'] += df[col]
-            elif params['oper']=='sub':
+            elif oper =='sub':
                 df['sub'] -= df[col]
-            elif params['oper']=='mul':
+            elif oper =='mul':
                 df['mul'] *= df[col]
-            elif params['oper']=='div':
+            elif oper =='div':
                 df['div'] /= df[col]
 
     return _helper.publish(df)
-        
-try:
-    if main() != None:
-        _helper.status(_helper.fileid, 2,'Completed')
-except Exception as e:
-    _helper.status(_helper.fileid, -2,str(e))
          
