811bb252d813ab551d8d8374a07f1b4e94f7f4be
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 11:34:54 +0530

    Update tf_idf.py

--- tf_idf.py
+++ tf_idf.py
@@ -1,7 +1,6 @@
 params ={'column' : }
 import _helper
-_helper.tablename='my new filename'
-
+#_helper.tablename='my new filename'
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
