caab1521a133f83debc4522c51a2894231d743d6
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 16:09:58 +0530

    Update box_cox.py

--- box_cox.py
+++ box_cox.py
@@ -6,12 +6,15 @@ import json
 def main():
     df = _helper.data()
     #Enter the column which has integer data type
-    if 'column' in df:
-        transform = df['column'].values
-        # transform values and store as "dft"
-        dft = stats.boxcox(transform)
-        df['box_cox'] = dft[0]
-        return _helper.publish(df)
+    col=''
+    if col in df:
+        if df[col].dtypes=='float64':
+            df[col]=df[col].astype(int)
+            transform = df[col].values
+            # transform values and store as "dft"
+            dft = stats.boxcox(transform)
+            df['box_cox'] = dft[0]
+            return _helper.publish(df)
     else:
         return 'Column does not exist'
 
