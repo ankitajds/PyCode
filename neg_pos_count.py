98615bc4e526d3368101b30bc7cb7a9c949b1e7f
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 15:35:36 +0530

    Update neg_pos_count.py

--- neg_pos_count.py
+++ neg_pos_count.py
@@ -1,6 +1,6 @@
-params =  {"var" : ""}
+#Enter the positive or negative or both in var
+var=''
 import _helper
-#_helper.tablename='my new filename'
 import pandas as pd
 import math
 import numpy as np
@@ -9,18 +9,13 @@ import json
 
 def main():
     data = _helper.data()
-    if params["var"]   == 'positive':
+    if var == 'positive':
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
-    elif params["var"] == 'negative':
+    elif var == 'negative':
         data['negative_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
-    elif params["var"] == 'both':
+    elif var == 'both':
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
 
     return _helper.publish(data)
         
-try:
-    if main() != None:
-        _helper.status(_helper.fileid, 2,'Completed')
-except Exception as e:
-    _helper.status(_helper.fileid, -2,str(e))
