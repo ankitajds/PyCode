924eea95433c599a996038756978a52795b160fa
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 15:46:08 +0530

    Update rolling_window.py

--- rolling_window.py
+++ rolling_window.py
@@ -1,12 +1,12 @@
 #Enter column names in the list defined below
-#column = [ ]
-#win size is the number of observations used for calculating the statistic.
+columns = [ ]
+#window size is the number of observations used for calculating the statistic.
+win_size=
 #Provide a window type like boxcar , hamming ,triang , blackman.
+window_type=''
 #Provide a operation like sum or mean.
-
-params={'columns':[],'window_size':,'window_type':,'operation': }
+operation=''
 import _helper
-#_helper.tablename='my new filename'
 import pandas as pd
 import numpy as np
 import re
@@ -16,21 +16,14 @@ import json
 
 def main():
     du = _helper.data()
-    for col in params['columns']:
-        if params['operation'] =="sum":
-            du[col+"_sum"] = du[col].rolling(params['window_size'],win_type = params['window_type']).sum()
-        elif params['operation'] =="mean":
-            du[col+"_mean"] = du[col].rolling(params['window_size'], win_type = params['window_type']).mean()
-
+    for col in columns :
+        if operation =="sum":
+            du[col+"_sum"] = du[col].rolling(window_size = win_size, win_type = window_type).sum()
+        elif operation =="mean":
+            du[col+"_mean"] = du[col].rolling(window_size = win_size, win_type = window_type).mean()
 
 
     return _helper.publish(du)
-        
-try:
-    if main() != None:
-        _helper.status(_helper.fileid, 2,'Completed')
-except Exception as e:
-    _helper.status(_helper.fileid, -2,str(e))
         
 
    
