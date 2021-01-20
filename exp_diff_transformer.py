b0846e1da62a1cebadc72015a75522433ab0e7db
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 17:45:08 +0530

    Update exp_diff_transformer.py

--- exp_diff_transformer.py
+++ exp_diff_transformer.py
@@ -1,19 +1,18 @@
 # enter the column name which has integer or float datatype
-# raised to the power m and n 
-
+column_1=''
+column_2=''
+#Enter the exponent values for the two columns
+m =
+n =
 import _helper
 import pandas as pd
 import math
 import json
 
-#Enter the exponent values for the two columns
-m =
-n =
-
 def main():
     data = _helper.data() 
-    if 'column_1' in data.columns and 'column_2' in data.columns : #Enter column 1 and column 2
-        data['exp'] = pow(data['column_1'],m]) - pow(data['column_2'],n])
+    if column_1 in data.columns and column_2 in data.columns : #Enter column 1 and column 2
+        data['exp'] = pow(data[column_1],m]) - pow(data[column_2],n])
         return _helper.publish(data)
     else:
         return None
