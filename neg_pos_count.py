27a57cc5c294fd6a79809c100ee58c94bb9bead7
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 18:01:19 +0530

    Update neg_pos_count.py

--- neg_pos_count.py
+++ neg_pos_count.py
@@ -11,11 +11,13 @@ def main():
     data = _helper.data()
     if var == 'positive':
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
+        return _helper.publish(data)
     elif var == 'negative':
         data['negative_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
+        return _helper.publish(data)
     elif var == 'both':
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').ge(0).sum(axis=1)
         data['positive_count'] = data.select_dtypes(include='int64' or 'float64').lt(0).sum(axis=1)
-
-    return _helper.publish(data)
-        
+        return _helper.publish(data)
+    else:
+        return None
