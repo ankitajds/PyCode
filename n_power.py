1af684ce2f7023111e39d1db5cfe3f18d9c083bd
easydatain <74894256+easydatain@users.noreply.github.com>
2021-01-20 17:57:16 +0530

    Update n_power.py

--- n_power.py
+++ n_power.py
@@ -1,22 +1,20 @@
-import _helper
-import pandas as pd
-import math
-import json
 #Enter column names in the list defined below
 columns=[]
 # pass the integer value in n variable for find square
 n=
+import _helper
+import pandas as pd
+import math
+import json
+
 def main():
     df = _helper.data()
     for col in columns:
-        try:
+        if col in df.columns:
             df[col+"_n_root"]=df[col]**n
             return _helper.publish(df)
-
-        except:
-            return 'Enter column names which have float  datatype'
-
-    return _helper.publish(df)
+        else:
+            return None
 
     
                     
