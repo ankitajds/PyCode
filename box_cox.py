d3ec093c9781a9a2a5bc753abf0ee1424ca28d56
easydatain <74894256+easydatain@users.noreply.github.com>
2021-03-02 13:38:25 +0530

    Update box_cox.py

--- box_cox.py
+++ box_cox.py
@@ -18,9 +18,11 @@ def main():
             # transform values and store as "dft"
             dft = stats.boxcox(transform)
             df['box_cox'] = dft[0]
-        return _helper.publish(df)
+        
     else:
         return None
+    
+    return _helper.publish(df)
 
     
         
