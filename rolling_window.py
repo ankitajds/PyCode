72c1a3675090cf21c09f9a85e91c0af54aaad908
easydatain <74894256+easydatain@users.noreply.github.com>
2021-03-02 15:45:12 +0530

    Update rolling_window.py

--- rolling_window.py
+++ rolling_window.py
@@ -19,9 +19,10 @@ def main():
     for col in columns :
         if operation =="sum":
             du[col+"_sum"] = du[col].rolling(win_size, win_type = window_type).sum()
-            return _helper.publish(du)
+            
         elif operation =="mean":
             du[col+"_mean"] = du[col].rolling(win_size, win_type = window_type).mean()
-            return _helper.publish(du)
+            
         else:
             return None
+    return _helper.publish(du)
