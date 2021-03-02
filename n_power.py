7f31dd437441df8be86ba24340b66940ecc5bab6
easydatain <74894256+easydatain@users.noreply.github.com>
2021-03-02 13:25:18 +0530

    Update n_power.py

--- n_power.py
+++ n_power.py
@@ -11,10 +11,12 @@ def main():
     df = _helper.data()
     for col in columns:
         if col in df.columns:
-            df[col+"_n_root"]=pd.to_numeric(df[col])**n
-            return _helper.publish(df)
+            df[col+"_n_root"]=df[col]**n
+            
         else:
             return None
+        
+     return _helper.publish(df)
 
     
                     
