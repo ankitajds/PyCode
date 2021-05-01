c8e3401aa4bfc1c43808b605ebeb0dd59812770d
easydatain <74894256+easydatain@users.noreply.github.com>
2021-05-01 19:03:51 +0530

    Update Power_transformer.py

--- Power_transformer.py
+++ Power_transformer.py
@@ -1,1 +1,29 @@
+import _helper
+from scipy import stats
+import pandas as pd
+import json
+from sklearn.preprocessing import PowerTransformer
+
+col=[]
+def main():
+    df = _helper.data()
+    for c in col:
+        if c in df:
+            df[c] =df[c].astype('int64')
+            features = df[[c]]
+            pt = PowerTransformer(method='yeo-johnson', standardize=True,) 
+            #Fit the data to the powertransformer
+            pt_yeojohnson = pt.fit(features)
+            #Transform the data 
+            pt_yeojohnson = pt.transform(features)
+            #Pass the transformed data into a new dataframe 
+            df_xt = pd.DataFrame(data=pt_yeojohnson, columns=[c + '_yeojohn'])
+            df=df.join(df_xt)
+            
+
+        else:
+            return None
+    
+    
+    return _helper.publish(df)
 
